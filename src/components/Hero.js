38ea33e79f72982fbc16fe91fb584c5ff80a1698
Raul Ospina <raminka13@hotmail.com>
2023-02-06 11:10:11 -0500

    Add Hero

---
+++ src/components/Hero.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Hero = () => (
+  <div className="container">
+    <h1 className="title">Raul Ospina</h1>
+  </div>
+);
+
+export default Hero;
---
+++ src/useElementOnScreen.js
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react';
+
+const useElementOnScreen = (options) => {
+  const containerRef = React.useRef(null);
+  const [isVisible, setIsVisible] = React.useState(false);
+
+  const callbackFunction = (entries) => {
+    const [entry] = entries;
+    setIsVisible(entry.isIntersecting);
+  };
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(callbackFunction, options);
+    if (containerRef.current) {
+      observer.observe(containerRef.current);
+    }
+    return () => {
+      if (containerRef.current) {
+        observer.unobserve(containerRef.current);
+      }
+    };
+  }, [containerRef, options]);
+
+  return [containerRef, isVisible];
+};
+
+export default useElementOnScreen;
